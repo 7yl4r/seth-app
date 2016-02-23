f2322fa934a2ebbc2c323cdf4f132feac60d59d6
7yl4r <murray.tylar@gmail.com>
2016-02-22 22:32:02 -0500

    ohhh, dropdown+text+link. not 2xdropdowns+link

--- dynamic-form-controller.js
+++ dynamic-form-controller.js
@@ -1,29 +1,19 @@
-$("#choice-1").on("change", function(){
-    console.log("choice-1 change");
-
-    var choice2 = $("#choice-2").empty();
-
-    var options = getChoice2Options($('#choice-1').val())
-    $.each(options, function(key, choiceObj){
-        choice2.append(
-            $('<option>', choiceObj, '</option>')
-        );
-    });
-});
-
-function getChoice2Options(choice1){
-    // returns set of options for choice 2 given choice 1 selection
-    // TODO: this is just a json lookup, change to that once I have the real values.
-    if (choice1 == "1-1"){
-        return [
-            {text:"test text apples", value: "appl"},
-            {text:"test text bananas", value: "bann"}
-        ];
-    } else if( choice1 == "1-2"){
-        return [
-            {text:"text text 1-2", value: "test1-2"}
-        ]
-    } else {
-        console.error("unknown value for choice-1:" + choice1);
+var data = {
+    "1-1": {
+        "text": "you chose 1-1",
+        "link": "#option1",
+        "linkText": "link for optn 1"
+    },
+    "1-2": {
+        "text": "1-2 chosen",
+        "link": "#option2",
+        "linkText": "link for op 2"
     }
 }
+
+$("#choice").on("change", function(){
+    console.log("choice change");
+    var cho = $("#choice").val();
+    $("#dynamic-text").html(data[cho].text);
+    $("#dynamic-link").html('<a href="' + data[cho].link + '">' + data[cho].linkText + "</a>");
+});
